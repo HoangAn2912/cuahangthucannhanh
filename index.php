69ca64fba7ae6463b5bd59a83bd2140e1827625e
nguyentrang2642003@gmail.com <nguyentrang2642003@gmail.com>
2024-10-25 21:49:16 +0700

    layout

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    error_reporting(1);
+    require("layout/header.php");
+    if(isset($_GET["page"]))
+        $page=$_GET["page"];
+    else
+        $page='trangchu';
+    
+    if(isset($_GET["cate"]))
+        $cate=$_GET["cate"];
+    if(file_exists("views/".$page."/index.php"))
+        include("views/".$page."/index.php");
+    else
+    include("views/404/index.php");
+?>
---
+++ views/trangchu/index.php
@@ -0,0 +1,16 @@
+<!-- Sidebar -->
+<?php
+    require("layout/navqlchuoi.php");
+?>
+<div class="sidebar">
+        <a href="#"><i class="fas fa-cogs"></i> Cài đặt</a>
+        <a href="#"><i class="fas fa-question-circle"></i> Hỗ trợ</a>
+    </div>
+
+    <div style="margin-left: 210px; padding: 20px;">
+        <h2>Nội dung chính</h2>
+        <p>Đây là nơi nội dung của trang sẽ xuất hiện...</p>
+    </div>
+
+</body>
+</html>
